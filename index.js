5241cc1f7573d35d7e0158f41e70eb601c7a3d25
JobLessGod <iamjoblessgod@gmail.com>
2025-03-13 05:33:57 +0545

    1.1.1

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import { execSync } from "child_process";
 import fs from "fs";
 import path from "path";
 
-// Get project name from command arguments
 const projectName = process.argv[2];
 
 if (!projectName) {
@@ -12,14 +10,11 @@ if (!projectName) {
 
 const projectPath = path.join(process.cwd(), projectName);
 
-// Function to copy template files
 function createProjectStructure() {
   console.log(`Creating project: ${projectName}`);
 
-  // Create project directory
   fs.mkdirSync(projectPath, { recursive: true });
 
-  // Create sample files
   fs.writeFileSync(path.join(projectPath, "index.js"), "// Entry file");
   fs.writeFileSync(path.join(projectPath, "README.md"), `# ${projectName}`);
 
